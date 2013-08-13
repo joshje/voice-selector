0c0520b349da9a8871645350d6c34b1105a901b5
Josh Emerson <josh@zeebox.com>
2013-08-13 16:01:44 +0100

    Use includes for bootstrap

--- assets/script.js
+++ assets/script.js
@@ -470,54 +470,14 @@ jQuery.extend( jQuery.easing,
 
 })(jQuery);
 ;(function($) {
-    var RESULTS = [
-        {
-            type: "twitter",
-            name: "anthonyrose",
-            image: "https://si0.twimg.com/profile_images/1599447732/Anthony1-small-square.JPG",
-            min: 500,
-            max: 5000
-        },
-        {
-            type: "blank",
-            image: "assets/images/blank-1.jpg"
-        },
-        {
-            type: "twitter",
-            name: "ErnestoSchmitt",
-            image: "https://si0.twimg.com/profile_images/1600852722/image.jpg",
-            min: 600,
-            max: 6000
-        },
-        {
-            type: "blank",
-            image: "assets/images/blank-2.jpg"
-        },
-        {
-            type: "twitter",
-            name: "mbleyleben",
-            image: "https://si0.twimg.com/profile_images/88472831/ImgSheet_8_cr.jpg",
-            min: 700,
-            max: 7000
-        },
-        {
-            type: "twitter",
-            name: "endotoh",
-            image: "https://si0.twimg.com/profile_images/1661445920/alex-profile-pic.jpg",
-            min: 800,
-            max: 8000
-        },
-        {
-            type: "multiplier",
-            value: 2,
-            image: "assets/images/multiplier-x2.jpg"
-        },
-        {
-            type: "multiplier",
-            value: 3,
-            image: "assets/images/multiplier-x3.jpg"
-        }
-    ];
+
+    var results;
+    $.ajax({
+        url: 'data/results.json'
+    }).done(function(data) {
+        results = data;
+        init();
+    });
 
     var slots = [];
     var multiplier = 1;
@@ -565,8 +525,8 @@ jQuery.extend( jQuery.easing,
 
     var init = function() {
         var slot = '<ul class="slot">' + "\n";
-        for (var i = 0, len = RESULTS.length; i < len; ++i) {
-            var result = RESULTS[i];
+        for (var i = 0, len = results.length; i < len; ++i) {
+            var result = results[i];
             slot += '<li>';
             if (result.image) {
                 slot += '<img src="' + result.image + '">';
@@ -605,7 +565,7 @@ jQuery.extend( jQuery.easing,
                 slots = [];
                 multiplier = 1;
                 for (var slot = numbers.length - 1; slot >= 0; slot--) {
-                    var result = RESULTS[numbers[slot]-1];
+                    var result = results[numbers[slot]-1];
                     if (result && result.type == 'twitter') {
                         slots[slot] = result.name;
                     } else {
@@ -621,7 +581,5 @@ jQuery.extend( jQuery.easing,
 
         $('.slots-play').click();
     };
-
-    init();
 
 })(jQuery);
